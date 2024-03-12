7a947de8fc1530db3d21f6f10c233afb72e39cfa
KavyamSachdeva <124245507+Kavyam172@users.noreply.github.com>
2024-03-12 10:38:45 +0530

    movie details page added, movie filter functionality added

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     // Simulate an API request or any async operation
     let top_box_office = await topBoxOffice();
     carouselPosters(top_box_office);
+    carousel();
     setTimeout(() => {
         hideLoader();
         showContent();
@@ -26,29 +27,37 @@ search.addEventListener('click', () => {
     let searchbar = document.querySelector(".search-bar");
     if (searchbar.classList.contains("active")) {
         let searchValue = document.querySelector(".search-bar input").value;
+        document.querySelector(".first").style.display = "block";
         showMovieData(searchValue);
+
+
+        let section = document.querySelector('#search-results');
+        section.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+
     }
     else {
         searchbar.classList.add("active");
     }
-    // alert('clicked');
 });
 
 
 
 //{carousel}
-const carousels = document.querySelectorAll(".carousel");
+function carousel() {
+    const carousels = document.querySelectorAll(".carousel");
 
-carousels.forEach(carousel => {
-    const carouselInner = carousel.querySelector(".images");
-    const carouselContent = Array.from(carouselInner.children);
-    carouselContent.forEach(item => {
-        const duplicatedItem = item.cloneNode(true);
-        carouselInner.appendChild(duplicatedItem);
-        carouselInner.style.animation = "move 50s linear infinite";
+    carousels.forEach(carousel => {
+        const carouselInner = carousel.querySelector(".images");
+        const carouselContent = Array.from(carouselInner.children);
+        carouselContent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true);
+            carouselInner.appendChild(duplicatedItem);
+            carouselInner.style.animation = "move 50s linear infinite ";
 
-    })
-});
+        })
+    });
+}
+
 
 
 //{top movies poster from API}
@@ -89,41 +98,41 @@ async function topBoxOffice() {
 
 //{search movies API}
 async function searchMovies(value) {
-    const url = `https://moviesdatabase.p.rapidapi.com/titles/search/title/${value}?exact=false&titleType=movie`;
+    const url = `https://moviesminidatabase.p.rapidapi.com/movie/imdb_id/byTitle/${value}/`;
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'e87193b36cmshb02ac7a2bf02a67p13322fjsn8ed7ba7f6346',
-            'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+            'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
         }
     };
 
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        return result["results"];
-        // console.log(result["results"]);
+        // console.log(result);
+        return result['results'];
     } catch (error) {
         console.error(error);
     }
 }
 
 //{get ratings from movie ID}
-async function getRatings(id='tt0848228'){
-    const url = 'https://moviesdatabase.p.rapidapi.com/titles/tt0848228/ratings';
+async function getMovieById(id='tt0848228'){
+    const url = `https://moviesminidatabase.p.rapidapi.com/movie/id/${id}/`;
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'e87193b36cmshb02ac7a2bf02a67p13322fjsn8ed7ba7f6346',
-            'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+            'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
         }
     };
 
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        console.log(result);
-        // return result;
+        // console.log(result);
+        return result['results'];
     } catch (error) {
         console.error(error);
     }
@@ -131,28 +140,24 @@ async function getRatings(id='tt0848228'){
 
 //{show movie data in cards}
 async function showMovieData(value){
-    // let cardPoster = document.querySelector('#movie-poster');
-    // let cardTitle = document.querySelector('#movie-title');
-    let results = document.querySelector('.search-results');
-    // console.log(results);
+    let results = document.querySelector('#results');
     let data = await searchMovies(value);
     console.log(data);
     let cards = "";
     
     for(let i in data){
         let x = data[i];
-        console.log(1);
+        // let x = await getMovieById(id);
 
-        if(x['primaryImage'] === null){
-            continue
-        }
-        cards += `<div class="flip-card">
+
+
+        cards += `<div class="flip-card" id = ${x['id']}>
                         <div class="flip-card-inner">
                             <div class="flip-card-front">
-                                <img id="movie-poster" src=${x['primaryImage']['url']} alt="${x['titleText']['text']}">
+                                <img id="movie-poster" src=https://image.tmdb.org/t/p/w500${x['poster_path']} alt="${x['title']}">
                             </div>
                             <div class="flip-card-back">
-                                <p class="title" id="movie-title">${x['titleText']['text']}</p>
+                                <p class="title" id="movie-title">${x['title']}</p>
                                 <p id = 'view-more'>View More</p>
                             </div>
                         </div>
@@ -160,3 +165,15 @@ async function showMovieData(value){
     }
     results.innerHTML = cards;
 }
+
+//{redirect to movie page}
+document.addEventListener('click', (e) => {
+    if(e.target.id === 'view-more'){
+        let id = e.target.parentElement.parentElement.parentElement.id;
+        redirect(id);
+    }
+});
+
+function redirect(id){
+    window.location.href = `movie_details.html?id=${encodeURIComponent(JSON.stringify(id))}`;
+};
